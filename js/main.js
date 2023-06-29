39223579c659d49a5e261c9bcdacd5667f55db37
Vlad Korpik <66820455+vlad-korpik-1992@users.noreply.github.com>
2023-06-29 11:13:16 +0300

    The single page is ready

--- js/main.js
+++ js/main.js
@@ -1,5 +1,58 @@
 $(document).ready(function() {
 
+    $('.related__box').slick({
+		mobileFirst: true,
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		rows: 1,
+		arrows: true,
+		dots: false,
+		autoplay: true,
+		infinite: true,
+    responsive: [
+      {
+        breakpoint: 990,
+        settings: {
+          slidesToShow: 5
+        }
+      },
+      {
+        breakpoint: 765,
+        settings: {
+          slidesToShow: 4
+        }
+      },
+      {
+        breakpoint: 570,
+        settings: {
+          dots: false,
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 420,
+        settings: {
+          dots: false,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 370,
+        settings: {
+          dots: false,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 310,
+        settings: {
+          dots: false,
+          slidesToShow: 1
+        }
+      }
+    ]
+	});
+
     $('.catalog__link').click(function (e) {
         e.preventDefault();
         let elem = e.target;
